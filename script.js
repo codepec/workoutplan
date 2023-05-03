cc58748c0d135a3012bf3c88160af809fea07c72
codepec <codepec.de@gmail.com>
2023-05-03 12:18:06 +0200

    first commit

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ const workoutDays = document.getElementById("workoutDays");
 
 workoutDays.addEventListener("change", function () {
   if (workoutDays.value === "threeDays") {
-    
   } else {
     alert("Workout-Pläne werden noch erstellt.");
   }
 });
 
-
 const chooseLanguage = document.getElementById("chooseLanguage");
 
 chooseLanguage.addEventListener("change", function () {
   if (chooseLanguage.value === "german") {
-    
   } else {
     alert("work in progress");
   }
