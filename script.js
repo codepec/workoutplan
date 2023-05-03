637fdf8505bd5697156585aa306dfcab93937eff
codepec <codepec.de@gmail.com>
2023-05-03 12:02:28 +0200

    add workoutplan

---
+++ script.js
@@ -0,0 +1,20 @@
+const workoutDays = document.getElementById("workoutDays");
+
+workoutDays.addEventListener("change", function () {
+  if (workoutDays.value === "threeDays") {
+    
+  } else {
+    alert("Workout-Pläne werden noch erstellt.");
+  }
+});
+
+
+const chooseLanguage = document.getElementById("chooseLanguage");
+
+chooseLanguage.addEventListener("change", function () {
+  if (chooseLanguage.value === "german") {
+    
+  } else {
+    alert("work in progress");
+  }
+});
