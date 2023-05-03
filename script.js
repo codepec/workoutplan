5db00f31e51e96906d2d1ff16073a7296f50a232
codepec <codepec.de@gmail.com>
2023-05-03 22:10:58 +0200

    Main

--- script.js
+++ script.js
@@ -15,3 +15,57 @@ chooseLanguage.addEventListener("change", function () {
     alert("work in progress");
   }
 });
+
+// Funktion zum Hinzufügen eines Termins
+
+function addCalendarEvent(title) {
+  // Datum erstellen
+
+  let date = new Date();
+
+  // Daten für das Kalenderereignis
+
+  let eventData = {
+    title: title,
+
+    start: date,
+
+    end: date,
+  };
+
+  // WebCal API aufrufen
+
+  if (navigator.webcal) {
+    navigator.webcal
+
+      .saveEvent(eventData)
+
+      .then(() => {
+        console.log("Ereignis wurde erfolgreich im Kalender gespeichert!");
+      })
+
+      .catch((error) => {
+        console.error("Fehler beim Speichern des Ereignisses: ", error);
+      });
+  } else {
+    console.error("WebCal API wird von diesem Browser nicht unterstützt!");
+  }
+}
+
+// Event-Listener für Brust-Button
+
+document.getElementById("brust").addEventListener("click", function () {
+  addCalendarEvent("Training Tag 1 - Brust - erfolgreich absolviert.");
+});
+
+// Event-Listener für Rücken-Button
+
+document.getElementById("ruecken").addEventListener("click", function () {
+  addCalendarEvent("Training Tag 2 - Beine - erfolgreich absolviert.");
+});
+
+// Event-Listener für Ganzkörper-Button
+
+document.getElementById("ganzkoerper").addEventListener("click", function () {
+  addCalendarEvent("Training Tag 3 - Ganzkörper - erfolgreich absolviert.");
+});
